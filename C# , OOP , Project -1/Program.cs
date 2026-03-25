fed9f9969dced236efe9849b392130d2d47978ec
Heyedos <hidosss@hotmail.com>
2026-03-25 17:02:08 +0300

    Add project files.

---
+++ C# , OOP , Project -1/Program.cs
@@ -0,0 +1,163 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace C____OOP___Project__1
+    {
+        internal class Program
+        {
+            interface IKayitEdilebilir
+            {
+                void Kaydet();
+            }
+            abstract class Material:IKayitEdilebilir
+            {
+                public string Ad{ get; private set; }
+                public int YayimYili { get; private set; }
+                public Material(string ad, int yayimYili)
+                {
+                    Ad = ad;
+                    YayimYili = yayimYili;
+                }
+            
+                public abstract void BilgiYaz();
+
+                public abstract void Kaydet();
+           
+            }
+            class Kitap:Material
+            {
+                public string Yazar { get; private set; }
+                public string ISBN { get; private set; }
+                public Kitap(string ad, int yayimYili, string yazar, string iSBN):base(ad,yayimYili)
+                {
+                    Yazar = yazar;
+                    ISBN = iSBN;
+                }
+
+                public override void BilgiYaz()
+                {
+                    Console.WriteLine($"Ad: {Ad} | Yayım Yılı: {YayimYili} | Yazar: {Yazar} | ISBN: {ISBN}");
+                }
+                public override void Kaydet()
+                {
+                    Console.WriteLine("Kitap Kaydedildi "+ Ad);
+                }
+            }
+            class Dergi:Material
+            {
+                public string SayiNO { get; private set; }
+
+                public Dergi(string ad, int yayimYili,string sayiNO):base(ad,yayimYili)
+                {
+                    SayiNO = sayiNO;
+                }
+
+                public override void BilgiYaz()
+                {
+                    Console.WriteLine($"Ad: {Ad} | Yayım Yılı: {YayimYili} | Sayi No: {SayiNO}");
+                }
+                public override void Kaydet()
+                {
+                    Console.WriteLine("Dergi Kaydedildi." + Ad);
+                }
+            }
+            class Uye:IKayitEdilebilir
+            {
+                public string Ad { get; private set; }
+                public string Email { get; private set; }
+                private static int _sayac;
+
+                public int UyeNo { get; private set; }
+
+                public Uye(string ad, string email)
+                {
+                    Ad = ad;
+                    Email = email;
+                    _sayac++;
+                    UyeNo = _sayac;
+                
+                }
+                public void Kaydet() { Console.WriteLine("Üye kaydedildi "+ Ad); }
+                public void BilgiYaz() { Console.WriteLine("Adı: "+Ad +" Email: "+ Email); }
+            }
+            class Kutuphane
+            {
+            private List<Material> materials;
+            private List<Uye> Uyes;
+
+                public Kutuphane()
+                {
+                    materials = new List<Material>();
+                    Uyes = new List<Uye>();
+                }
+
+                public void AddMaterial(Material m)
+                {
+                    try
+                    {
+                    materials.Add(m);
+                    Console.WriteLine("Materyal eklendi");
+                    m.Kaydet();
+                    }
+                    catch (Exception ex)
+                    {
+                    Console.WriteLine("Hata oluştu (Material): " + ex.Message);
+                    }
+                }
+                public void addUye(Uye U)
+                {
+                    try
+                    {
+                    Uyes.Add(U);
+                    Console.WriteLine("Üye eklendi");
+                    U.Kaydet();
+                    }
+                    catch (Exception ex)
+                    {
+                    Console.WriteLine("Hata oluştu (Üye): " + ex.Message);
+                    }
+                }
+                public void ListMaterials()
+                {
+                    foreach (Material m in materials)
+                    {
+                        m.BilgiYaz();
+                    }
+                }
+                public void ListUyes()
+                {
+                    foreach (Uye u in Uyes)
+                    {
+                        u.BilgiYaz();
+                    }
+                }
+            }
+            static void Main(string[] args)
+            {
+            try
+            {
+                Kutuphane K = new Kutuphane();
+
+                Kitap k1 = new Kitap("savaş", 2002, "ali", "10202345");
+                Dergi D1 = new Dergi("spidey", 2012, "155012");
+
+                Uye U1 = new Uye("ahmet", "ahmet@user.com");
+
+                K.AddMaterial(D1);
+                K.AddMaterial(k1);
+                K.addUye(U1);
+
+                K.ListMaterials();
+                K.ListUyes();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Genel hata: " + ex.Message);
+            }
+        }
+        }
+    }
---
+++ C# , OOP , Project -1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("C# , OOP , Project -1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("C# , OOP , Project -1")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("38b91f53-fe05-47fa-a78c-0042f2391d43")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
